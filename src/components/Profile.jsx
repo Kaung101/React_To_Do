2927388eac12a6e41b4e2bfed7b78a98415d6d00
kaung101 <kkstt.kaungkaung@gmail.com>
2023-03-26 10:08:05 +0700

    To_Do

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import "./Profile.css";
+function Profile (){
+    return(
+        <div className="layout">
+            <img src="assets/logo192.png" alt="logo"/>
+            <h1 className="name">React Logo</h1>
+            <hr></hr>
+        </div>
+    )
+}
+
+export default Profile;
